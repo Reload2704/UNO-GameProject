ac070b02cebaebfb75f134cf7e45a07357eec2aa
greenkarla <ktvelasc@espol.edu.ec>
2024-07-01 18:16:28 -0500

    añadi un metodo para robar carta

--- uno-game/src/main/java/ec/edu/espol/Baraja.java
+++ uno-game/src/main/java/ec/edu/espol/Baraja.java
@@ -1,5 +1,18 @@
 package ec.edu.espol;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Baraja {
-    
+    private ArrayList<Carta> cartas;
+   
+    public void barajar(){
+        Collections.shuffle(cartas);
+         // esto nos servira para mezclar las cartas de la baraja
+    }
+
+    public Carta robarCarta(){
+        /// aqui se roba la carta de la baraja y se la añade al jugador de quien disponga 
+        return Carta;
+    }
+
 }
