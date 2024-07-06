ddf7c713838ddc8d5e3b3bf42f9c1f1a66c721ca
Reload2704 <joranbra@espol.edu.ec>
2024-07-06 11:10:20 -0500

    Actualización Clase Juego

--- uno-game/src/main/java/ec/edu/espol/Juego.java
+++ uno-game/src/main/java/ec/edu/espol/Juego.java
@@ -1,60 +1,37 @@
 package ec.edu.espol;
 import java.util.ArrayList;
-import java.util.Collections;
-
 
 public class Juego {
-    private ArrayList<Carta> baraja = new ArrayList<>();
-    //private Jugador jugador;
-    //private Jugador maquina;
-    private ArrayList<Carta> lineaDeJuego = new ArrayList<>();
-    private boolean turno;
-
+    private ArrayList<Carta> baraja;
+    private Jugador jugador;
+    private Jugador maquina;
+    private ArrayList<Carta> lineaDeJuego;
+    
     public Juego(){
-        //  CREACION DE LA BARAJA (Bloque de codigo candidato a ponerlo en Utilitaria)
-        for(int a = 0; a<10; a++){
-            Carta cartaRojo = new CartaNumerica(Color.ROJO, a);
-            baraja.add(cartaRojo);
-        }
-        for(int e= 0; e<10; e++){
-            Carta cartaAmarillo = new CartaNumerica(Color.AMARILLO, e);
-            baraja.add(cartaAmarillo);
-        }
-        for(int o = 0; o<10;o++){
-            Carta cartaAzul = new CartaNumerica(Color.AZUL, o);
-            baraja.add(cartaAzul);
-        }
-        for( int u = 0; u<10; u++){
-            Carta cartaVerde = new CartaNumerica(Color.VERDE, u);
-            baraja.add(cartaVerde);
-        }
-        TipoEspecial [] especial = {TipoEspecial.REVERSE,TipoEspecial.BLOQUEO,TipoEspecial.MAS2,TipoEspecial.MAS4};
-        Color [] colores = {Color.ROJO,Color.AMARILLO,Color.VERDE,Color.AZUL};
-        for(int ba = 1; ba<3; ba++){
-            for(Color color: colores){
-                for(TipoEspecial cardEspecial : especial){
-                    Carta newespecial = new CartaEspecial(color, cardEspecial);
-                    baraja.add(newespecial);
-                }
-            }
-        }
-        TipoEspecial [] cambiocolor = {TipoEspecial.CAMBIO_DE_COLOR, TipoEspecial.MAS2, TipoEspecial.MAS4};
-        Color [] negro = {Color.NEGRO};
-        for(int da = 1; da<3; da++){
-            for(Color extra: negro){
-                for(TipoEspecial comodin: cambiocolor){
-                    Carta cardcomodin = new CartaEspecial(extra, comodin);
-                    baraja.add(cardcomodin);
-                }
-            }
-        }
-        Collections.shuffle(baraja); //Baraja las cartas
-        lineaDeJuego.add(baraja.remove(0)); //Aqui podria ir un indice random.
+        
     }
 
+    public void setBaraja(ArrayList<Carta> baraja) {
+        this.baraja = baraja;
+    }
+    
+    
+    public void setJugador(Jugador jugador) {
+        this.jugador = jugador;
+    }
+    
+    
+    public void setMaquina(Jugador maquina) {
+        this.maquina = maquina;
+    }
+
+
+    public void setLineaDeJuego(ArrayList<Carta> lineaDeJuego) {
+        this.lineaDeJuego = lineaDeJuego;
+    }
+    
     @Override
     public String toString() {
         return "Baraja ("+baraja.size()+"): "+ baraja + "\n\nLinea de Juego: " + lineaDeJuego;
     }
-    
 }
