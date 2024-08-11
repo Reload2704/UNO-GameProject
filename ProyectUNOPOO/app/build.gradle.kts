b0b58a4c7a72a65d206d3b95384cf9bbc6e10a66
greenkarla <ktvelasc@espol.edu.ec>
2024-08-11 03:42:48 -0500

    Proyect/Uno/firebase
    Actualización del proyecto/interfaz_login/firebase

--- ProyectUNOPOO/app/build.gradle.kts
+++ ProyectUNOPOO/app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.googleService)
 }
 
 android {
@@ -37,6 +38,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebaseAuth)
+    implementation(libs.firebaseStorage)
+    implementation(libs.firebaseStore)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
